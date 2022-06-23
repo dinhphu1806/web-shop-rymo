bc68e8eb4e371714fd9cdee1e01d85ca5ea7a65c
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-06-23 12:46:52 +0700

    initital commit last

--- js/bar.js
+++ js/bar.js
@@ -1,19 +1,8 @@
 const navBar = document.querySelector('.navbar');
 const menuBtn = document.querySelector('#menu-btn');
 
-const user = document.querySelector('#user');
-const userSub = document.querySelector('.user-sub');
-const userList = document.querySelector('.user-list')
 
 menuBtn.onclick = () => {
    menuBtn.classList.toggle('fa-times');
    navBar.classList.toggle('active');
 }
-
-user.onclick = () => {
-   userSub.classList.toggle('show');
-}
-
-userList.addEventListener('click', function(event) {
-   event.stopPropagation()
-})
---
+++ js/modalSearch.js
@@ -0,0 +1,42 @@
+const searchForm = document.querySelector('.search-form');
+const cartForm = document.querySelector('#cart-form');
+
+//serach-btn
+const searchBtn = document.querySelector('#search-btn');
+
+searchBtn.onclick = () => {
+    searchForm.classList.toggle('show');
+    // cartForm.classList.remove('show')
+}
+
+// cart-btn
+const cartBtn = document.querySelector('#cart-btn');
+
+cartBtn.onclick = () => {
+    cartForm.classList.toggle('active')
+}
+
+// window
+// window.onscroll = () => {
+//     searchForm.classList.remove('show');
+//     cartForm.classList.remove('show');
+// }
+
+// cart-btn
+// const cart = document.querySelector('.cart');
+// const cartBtns = document.querySelector('#cart-btn');
+// const cartClose = document.querySelector('.cart-close-js');
+// const cartContainer = document.querySelector('.cart-container-js');
+
+// function showCart() {
+//     cart.classList.add('open')
+// }
+
+// function hideCart() {
+//     cart.classList.remove('open')
+// }
+
+// for(const cartBtn of cartBtns) {
+//     cartBtn.addEventListener('click', showCart)
+// }
+
