1701a8981c149a056a7e5b7d589cd3f41a8f3e4e
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-06-25 02:21:18 +0700

    initial commit today

---
+++ src/js/backToTop.js
@@ -0,0 +1,9 @@
+const btnTop = document.querySelector('.btn-top');
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 200) {
+        btnTop.classList.add('show')
+    } else {
+        btnTop.classList.remove('show')
+    };
+})
