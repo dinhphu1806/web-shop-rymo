0b3490414e63da830a681e9250a7bb8ee2037a8b
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-07-01 00:22:02 +0700

    ok

--- src/js/bar.js
+++
@@ -1,8 +0,0 @@
-// const navBar = document.querySelector('.navbar');
-// const menuBtn = document.querySelector('#menu-btn');
-
-
-// menuBtn.onclick = () => {
-//    menuBtn.classList.toggle('fa-times');
-//    navBar.classList.toggle('active');
-// }
