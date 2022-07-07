b930d639754412b016bf40a8f33d262e9d343a00
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-07-07 20:47:23 +0700

    okoko

--- src/js/modalSearch.js
+++ src/js/modalSearch.js
@@ -1,40 +1,40 @@
-const searchForm = document.querySelector('.search-form');
-const cartForm = document.querySelector('.cart-form');
+// const searchForm = document.querySelector('.search-form');
+// const cartForm = document.querySelector('.cart-form');
 
 
-//serach-btn
-const searchBtn = document.querySelector('#search-btn');
+// //serach-btn
+// const searchBtn = document.querySelector('#search-btn');
 
-searchBtn.onclick = () => {
-    searchForm.classList.toggle('show');
-    cartForm.classList.remove('show')
-}
+// searchBtn.onclick = () => {
+//     searchForm.classList.toggle('show');
+//     cartForm.classList.remove('show')
+// }
 
-// cart-btn
-const cartBtn = document.querySelector('#cart-btn');
+// // cart-btn
+// const cartBtn = document.querySelector('#cart-btn');
 
-cartBtn.onclick = () => {
-    searchForm.classList.remove('show')
-    cartForm.classList.toggle('show')
-}
-// bars
-const navBar = document.querySelector('.navbar');
-const menuBtn = document.querySelector('#menu-btn');
+// cartBtn.onclick = () => {
+//     searchForm.classList.remove('show')
+//     cartForm.classList.toggle('show')
+// }
+// // bars
+// const navBar = document.querySelector('.navbar');
+// const menuBtn = document.querySelector('#menu-btn');
 
-menuBtn.onclick = () => {
-    menuBtn.classList.toggle('fa-times');
-    navBar.classList.toggle('active');
-    searchForm.classList.remove('show');
-    cartForm.classList.remove('show')
- }
-// khi lăn chuột thì sẽ mất
+// menuBtn.onclick = () => {
+//     menuBtn.classList.toggle('fa-times');
+//     navBar.classList.toggle('active');
+//     searchForm.classList.remove('show');
+//     cartForm.classList.remove('show')
+//  }
+// // khi lăn chuột thì sẽ mất
 
-window.onscroll = () => {
-    searchForm.classList.remove('show');
-    cartForm.classList.remove('show');
-    navBar.classList.remove('active')
-}
-
+// window.onscroll = () => {
+//     searchForm.classList.remove('show');
+//     cartForm.classList.remove('show');
+//     navBar.classList.remove('active')
+// }
+// ----------------------------------------------------
 // cart-btn
 // const cart = document.querySelector('.cart');
 // const cartBtns = document.querySelector('#cart-btn');
@@ -52,4 +52,30 @@ window.onscroll = () => {
 // for(const cartBtn of cartBtns) {
 //     cartBtn.addEventListener('click', showCart)
 // }
+
+const btnSearchs = document.querySelector('#search-pc');
+const modalSearch = document.querySelector('.js-modal-search');
+const modalContainerSearch = document.querySelector('.js-modal-container-search');
+const modalCloseSearch = document.querySelector('.js-modal-close-search');
+
+
+function showModalSearch() {
+    modalSearch.classList.add('open');
+}
+
+function hideModalSearch(){
+    modalSearch.classList.remove('open');
+}
+// for(const btnSearch of btnSearchs){
+//     btnSearch.addEventListener('click', showModalSearch);
+// }
+btnSearchs.addEventListener('click', showModalSearch);
+
+modalSearch.addEventListener('click', hideModalSearch);
+modalCloseSearch.addEventListener('click', hideModalSearch);
+
+modalContainerSearch.addEventListener('click', function(event) {
+    event.stopPropagation();
+})
+
 
