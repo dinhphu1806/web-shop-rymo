a46df68b3851e6a2602181519cbb2a4dc9f75f2e
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-07-14 09:00:01 +0700

    initial commit quantity

--- src/js/backToTop.js
+++ src/js/backToTop.js
@@ -1,9 +1,9 @@
-const btnTop = document.querySelector('.btn-top');
+const btnTop = document.querySelector(".btn-top");
 
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 200) {
-        btnTop.classList.add('show')
-    } else {
-        btnTop.classList.remove('show')
-    };
-})
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 200) {
+    btnTop.classList.add("show");
+  } else {
+    btnTop.classList.remove("show");
+  }
+});
--- src/js/filterWatch.js
+++ src/js/filterWatch.js
@@ -1,34 +1,31 @@
-const filterBtns = document.querySelectorAll('.filter-btn');
-const filterItems = document.querySelectorAll('.filter-item');
-
+const filterBtns = document.querySelectorAll(".filter-btn");
+const filterItems = document.querySelectorAll(".filter-item");
 
 function showContentFilter(btn) {
-    filterItems.forEach((item) => {
-        if(item.classList.contains(btn.id)) {
-            removeFilterBtn();
-            btn.classList.add('active-btn');
-            item.style.display = "block";
-        } else {
-            item.style.display = "none";
-        }
-
-    })
+  filterItems.forEach((item) => {
+    if (item.classList.contains(btn.id)) {
+      removeFilterBtn();
+      btn.classList.add("active-btn");
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
 filterBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        showContentFilter(btn);
-    })
-})
-
+  btn.addEventListener("click", () => {
+    showContentFilter(btn);
+  });
+});
 
 function removeFilterBtn() {
-    filterBtns.forEach((btn) => {
-        btn.classList.remove('active-btn')
-    });
+  filterBtns.forEach((btn) => {
+    btn.classList.remove("active-btn");
+  });
 }
 
 // gach duoi dau tien
-window.addEventListener('DOMContentLoaded', () =>{
-    filterBtns[0].classList.add('active-btn')
-})
+window.addEventListener("DOMContentLoaded", () => {
+  filterBtns[0].classList.add("active-btn");
+});
