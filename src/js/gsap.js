20a04898c8eba7a5ee89e0a0ef045950be3ace10
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-07-18 17:02:25 +0700

    ppkpkpk

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -2,6 +2,6 @@ const timeline = gsap.timeline({default: {duration: 1}})
 
 timeline
     // .from('.header', {y: '-100%', ease: 'slow'})
-    .from('.menu-item', {opacity: 0, stagger: .1})
+    .from('.menu-item', {opacity: 0, stagger: .3})
     .from('.filter-item',{opacity: 0, stagger: .3})
     .from('.product-item',{opacity: 0, stagger: .3})
